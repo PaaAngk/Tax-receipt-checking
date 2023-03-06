cf39923d36873129b31cd7875e6b099b4aa14cee
pa <MrDeduchka@yandex.ru>
2023-03-06 19:58:06 +0800

    edit rec qr: add sharpness

--- qr_recognition.py
+++ qr_recognition.py
@@ -1,44 +1,19 @@
 import cv2
 from pyzbar import pyzbar
+from pyzbar.pyzbar import ZBarSymbol
 import numpy as np
 import streamlit as st
 import PyPDF2
 import PIL
 import torch
 import io
+from PIL import Image, ImageOps, ImageEnhance, ImageDraw, ImageFont
 # from kraken import binarization
+#https://kdmurray.id.au/post/2022-03-21_decode-qrcodes/
 
+#TODO: add ratate with sharp
 torch.hub._validate_not_a_forked_repo=lambda a,b,c: True
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='./best_6.pt', force_reload=True)
-
-def prepare_image(image):
-  original = image.copy()
-  gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-  blur = cv2.GaussianBlur(gray, (9,9), 0)
-  thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
-
-  # Morph close
-  kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-  close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
-
-  # Find contours and filter for QR code
-  cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-  cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-  ROI = []
-  for c in cnts:
-    peri = cv2.arcLength(c, True)
-    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-    x,y,w,h = cv2.boundingRect(approx)
-    area = cv2.contourArea(c)
-    ar = w / float(h)
-    if len(approx) == 4 and area > 1000 and (ar > .9 and ar < 1.3):
-      cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 3)
-      ROI = original[y:y+h, x:x+w]
-  if len(ROI)>0:
-    return ROI
-  else:
-    return image
-
 
 def decode_qr_data(texts):
   try:
@@ -81,32 +56,40 @@ def concat_image(img):
   imgs_comb = np.hstack([i.resize(min_shape) for i in images])
   return imgs_comb
 
+def sharpen(image, amount=1):
+  sharpener = ImageEnhance.Sharpness(image)
+  return sharpener.enhance(amount)
+
+def rotate(image, rot=30):
+  return image.copy().rotate(rot, expand=1)
 
 def read_qr(img_row, print_result = True):
-  # img = prepare_image(img)
-
-  detection_qr = get_prediction(img_row)
+  
+  detection_qr = qr_nn_rec(img_row)
+  #Getting only croped image from yolo 
   cropped_detection_qr = [x['im'] for x in detection_qr.crop()]
+  # Concate image if check exist two qr
   if len(cropped_detection_qr)>1:
     img = concat_image(cropped_detection_qr)
     # st.image(img)
   else:
     img = cropped_detection_qr[0]
-  st.image
-  st.image(img)
-
-  img = img[:, :, ::-1].copy()
-  scanned_qr = pyzbar.decode(img)
   
-  # st.image(detection_qr.render()[0])
+  img = Image.fromarray(img)
+  st.image(img)
+  # img = img[:, :, ::-1].copy()
+  scanned_qr = pyzbar.decode(img, [ZBarSymbol.QRCODE])
+  
   format_decode = formating_decode_qr_result(scanned_qr, img, print_result)
   if format_decode["warn"] == "can not read":
-    for i in range(4):
-      img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
-      scanned_qr = pyzbar.decode(img)
+    for sharpness, rotate_angle in zip([0.8, 1.5, 2], [10,20,30]):
+
+      image_sharpened = rotate(sharpen(img, sharpness), 20)
+      scanned_qr = pyzbar.decode(image_sharpened, [ZBarSymbol.QRCODE])
       format_decode = formating_decode_qr_result(scanned_qr, img, print_result)
-      st.write("Rotate")
-      st.image(img)
+      
+      st.write("sharpen")
+      st.image(image_sharpened)
       if format_decode["data"] is not None:
         st.write("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
         st.image(format_decode["image"])
@@ -140,10 +123,9 @@ def formating_decode_qr_result(scanned_qr, image, print_result = True):
     readed_image["data"] = None
   return readed_image
 
-
 def run_read_image(img):
   pil_image = PIL.Image.open(img)
-  nn = get_prediction(pil_image)
+  nn = qr_nn_rec(pil_image)
   return read_qr(nn.crop()[0]['im'])
 
 # Return all photo from pdf document with its name
@@ -157,12 +139,46 @@ def get_image_from_pdf(document):
       images.append({
         "image" : PIL.Image.open(io.BytesIO(image_file_object.data)),
         "name" : image_file_object.name
-      })
+      }) 
       count += 1
   return images
 
 
-def get_prediction(img):
+def qr_nn_rec(img):
   model.conf = 0.56
   results = model(img)
   return results
+
+
+
+
+
+
+# original = image.copy()
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# blur = cv2.GaussianBlur(gray, (9,9), 0)
+# thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
+
+# # Morph close
+# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+# close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+
+# return thresh
+
+# # Find contours and filter for QR code
+# cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+# ROI = []
+# for c in cnts:
+#   peri = cv2.arcLength(c, True)
+#   approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+#   x,y,w,h = cv2.boundingRect(approx)
+#   area = cv2.contourArea(c)
+#   ar = w / float(h)
+#   if len(approx) == 4 and area > 1000 and (ar > .9 and ar < 1.3):
+#     cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 3)
+#     ROI = original[y:y+h, x:x+w]
+# if len(ROI)>0:
+#   return ROI
+# else:
+#   return image
